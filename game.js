8994e147a130e934ba58caa1a3b11e1ec683638d
StudentAM <studentam@Administrators-MacBook-Pro-2.local>
2023-10-25 10:27:37 -0700

    java added

---
+++ game.js
@@ -0,0 +1,11 @@
+let g = Math.floor(Math.random() * 26) 
+
+let x = document.getElementById("guessInput").value
+
+let correctIncorrect = document.getElementById("guessText")
+
+document.getElementById("submitButton").onclick = function () {
+
+
+
+
