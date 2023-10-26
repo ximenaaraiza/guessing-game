0bd2fd3c095f539d35f9b64e7a0b1368dd550df6
StudentAM <studentam@Administrators-MacBook-Pro-2.local>
2023-10-26 10:29:47 -0700

    hello

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
-let g = Math.floor(Math.random() * 26) 
+let g = Math.floor(Math.random() * 26);
+let responseChange = document.getElementById("guessText");
+let goThrough = document.getElementById("submitButton");
+let input = document.getElementById("guessInput")
 
-let x = document.getElementById("guessInput").value
-
-let correctIncorrect = document.getElementById("guessText")
 
 document.getElementById("submitButton").onclick = function () {
+    let x = document.getElementById("guessInput").value;
+    if (x == g){
+        console.log("Correct. Good Job!!!");
+    }
 
+    else if(x > g){
+        console.log("TO HIGH. Guess again :(");
+    }
 
+    else (x < g);{
+        console.log("TO LOW. Guess again :(");
+    }
 
-
+}
