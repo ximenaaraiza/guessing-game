017ef91e46c60960d8d480bf669c0f07f45a02cd
StudentAM <studentam@Administrators-MacBook-Pro-2.local>
2023-10-27 09:57:39 -0700

    yay

--- game.js
+++ game.js
@@ -1,21 +1,29 @@
-let g = Math.floor(Math.random() * 26);
+let randomNumber = Math.floor(Math.random() * 26);
 let responseChange = document.getElementById("guessText");
 let goThrough = document.getElementById("submitButton");
-let input = document.getElementById("guessInput")
+let input = document.getElementById("guessInput");
+console.log( randomNumber)
 
 
-document.getElementById("submitButton").onclick = function () {
-    let x = document.getElementById("guessInput").value;
-    if (x == g){
-        console.log("Correct. Good Job!!!");
-    }
+function adivina (){
+    let userGuess = input.value;   
+      if (userGuess < randomNumber){ 
+        responseChange.innerText=" TOO LOW. Guess again :(";
+        responseChange.style.color= 'red'
+      }
 
-    else if(x > g){
-        console.log("TO HIGH. Guess again :(");
-    }
+      else if (userGuess > randomNumber){
+        responseChange.innerText= "TOO HIGH. Guess again :(";
+        responseChange.style.color= 'red'
+      }
 
-    else (x < g);{
-        console.log("TO LOW. Guess again :(");
-    }
-
+      else {
+        responseChange.innerText= "Correct!!:)";
+        responseChange.style.color= 'green'; 
+      }
 }
+goThrough.addEventListener("click",adivina )
+
+
+
+
